a3d634173af34e85a9a37c40f0d568a39a2ce0ea
Matthew Reyes and Stewart Cole <stewart@>
2016-08-01 16:35:57 -0700

    add simon project files

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,51 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function Simon() {
+  this.gameArray = [];
+  this.playerArray = [];
+  this.turn = 1;
+}
+
+Simon.prototype.loseCheck = function() {
+  for(var i = 0; i < this.playerArray.length; i++) {
+    if (this.playerArray[i] === this.gameArray[i]) {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+  };
+
+Simon.prototype.resetGame = function() {
+  this.gameArray = [];
+  this.playerArray = [];
+  this.turn = 1;
+  return this;
+};
+
+Simon.prototype.selectRandom = function() {
+  pieceArray = ["red", "green", "blue", "yellow"];
+  this.gameArray.push(pieceArray[Math.floor(Math.random() * 4)]);
+};
+
+Simon.prototype.patternDisplay = function() {
+  // debugger;
+  for (var i = 0; i < this.gameArray.length; i++) {
+    if (i >= this.gameArray.length) {
+      // clearTimeout(interval);
+    }
+    setTimeout(function(){}, 500);
+    clearTimeout();
+    $("#"+this.gameArray[i]+"-button").addClass('light-'+this.gameArray[i]);
+    setTimeout(function() {
+      $("#" + this.gameArray[i] + "-button").removeClass('light-' + this.gameArray[i])}, 500);
+  }
+};
+
+exports.simonModule = Simon;
+
+},{}],2:[function(require,module,exports){
 var Simon = require('./../js/simon.js').simonModule;
 
 $(document).ready(function(){
@@ -85,3 +133,5 @@ $(document).ready(function(){
     console.log(newSimonGame.turn);
   });
 });
+
+},{"./../js/simon.js":1}]},{},[2]);
--- js/simon.js
+++ js/simon.js
@@ -1,10 +1,40 @@
+function Simon() {
+  this.gameArray = [];
+  this.playerArray = [];
+  this.turn = 1;
+}
 
+Simon.prototype.loseCheck = function() {
+  for(var i = 0; i < this.playerArray.length; i++) {
+    if (this.playerArray[i] === this.gameArray[i]) {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+  };
 
+Simon.prototype.resetGame = function() {
+  this.gameArray = [];
+  this.playerArray = [];
+  this.turn = 1;
+  return this;
+};
 
+Simon.prototype.selectRandom = function() {
+  pieceArray = ["red", "green", "blue", "yellow"];
+  this.gameArray.push(pieceArray[Math.floor(Math.random() * 4)]);
+};
 
-
-
-
-
+Simon.prototype.patternDisplay = function() {
+  // debugger;
+  for (var i = 0; i < this.gameArray.length; i++) {
+    $("#"+this.gameArray[i]+"-button").addClass('light-'+this.gameArray[i]);
+    setTimeout(function() {
+      $("#" + this.gameArray[i] + "-button").removeClass('light-' + this.gameArray[i])}, 500);
+  }
+};
 
 exports.simonModule = Simon;
